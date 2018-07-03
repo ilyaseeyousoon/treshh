5a500830273a2f35f858e9b07263f32f625fb19f
ilyaseeyousoon <iljadlnv@gmail.com>
2018-07-03 15:26:54 +0300

    скрипт матлаб исправил

--- DSFD_v4/de0_iii_tdc/matlab_scripts/de0_work.m
+++ DSFD_v4/de0_iii_tdc/matlab_scripts/de0_work.m
@@ -7,7 +7,7 @@ buf_size=50000;
 
 s1 = serial('COM17','DataBits',8); 
 s1.InputBufferSize = buf_size; 
-s1.BaudRate = 781250; 
+s1.BaudRate = 1000000; 
 s1.Timeout=6; 
 s1.StopBits =2; 
 d=0;
@@ -19,7 +19,7 @@ temp = fread(s1);
 fclose(s1)
 
 for i=1:buf_size
-    if(i>40)
+    if(i>60)
     if (temp(i)==123)
     
        h=i; 
@@ -33,10 +33,10 @@ end
 
 % z(buf_size)=0;
 m=1;
-for j=h:4:(buf_size-50-d)
+for j=h:3:(buf_size-50-d)
     
- z(m)=temp(j+1)*65536+temp(j+2)*256+temp(j+3);
-z(m)=z(m)-15600000;
+ z(m)=temp(j+1)*256+temp(j+2);
+z(m)=z(m);
 if(z(m)<0)
     z(m)=0;
 end
